c9278b555dc54520bcef889ae62831a3f34951c7
dushiyu <820145912@qq.com>
2026-09-02 17:04:13 +0800

    添加fdcan

--- bootloader/boot_can_protocol.c
+++ bootloader/boot_can_protocol.c
@@ -1,6 +1,9 @@
 #include "boot_can_protocol.h"
 #include "boot_can_config.h"
 #include "boot_can_port.h"
+#include "fdcan.h"
+
+
 
 static uint8_t g_node_id;
 
@@ -79,4 +82,46 @@ void BootCAN_Process(uint8_t *data, uint8_t len) {
 
     break;
   }
+}
+
+void BootCAN_SendResponse(uint8_t cmd, uint8_t status, uint8_t *data) {
+
+  uint8_t tx[8] = {0};
+
+  tx[0] = g_node_id;
+
+  tx[1] = cmd;
+
+  tx[2] = status;
+
+  if (data != NULL) {
+    tx[3] = data[0];
+    tx[4] = data[1];
+    tx[5] = data[2];
+    tx[6] = data[3];
+  }
+
+  tx[7] = 0; // 暂时CRC不用
+
+  FDCAN_TxHeaderTypeDef txHeader;
+
+  txHeader.Identifier = BOOT_CAN_RESP_BASE_ID + g_node_id;
+
+  txHeader.IdType = FDCAN_STANDARD_ID;
+
+  txHeader.TxFrameType = FDCAN_DATA_FRAME;
+
+  txHeader.DataLength = FDCAN_DLC_BYTES_8;
+
+  txHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+
+  txHeader.BitRateSwitch = FDCAN_BRS_OFF;
+
+  txHeader.FDFormat = FDCAN_CLASSIC_CAN;
+
+  txHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+
+  txHeader.MessageMarker = 0;
+
+  HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &txHeader, tx);
 }
